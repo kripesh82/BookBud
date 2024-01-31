c11ec5e46bb63f4122d15a0be074c07e9dcfcd49
kripesh82 <pkripesh345@gmail.com>
2024-01-31 22:08:47 +0545

    initial commit with homepage and details page

---
+++ backend/spring-boot-library/src/main/java/com/bookbud/springbootlibrary/requestmodels/ReviewRequest.java
@@ -0,0 +1,14 @@
+package com.bookbud.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+import java.util.Optional;
+
+@Data
+public class ReviewRequest {
+    private double rating;
+
+    private Long bookId;
+
+    private Optional<String> reviewDescription;
+}
