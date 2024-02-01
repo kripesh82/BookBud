2e6225c8f5879cac2dc2b0a364d29ce7f05a28aa
kripesh82 <pkripesh345@gmail.com>
2024-02-01 11:17:30 +0545

    messages feature added

--- backend/spring-boot-library/src/main/java/com/bookbud/springbootlibrary/config/DataRestConfig.java
+++ backend/spring-boot-library/src/main/java/com/bookbud/springbootlibrary/config/DataRestConfig.java
@@ -2,6 +2,7 @@ package com.bookbud.springbootlibrary.config;
 
 
 import com.bookbud.springbootlibrary.entity.Book;
+import com.bookbud.springbootlibrary.entity.Message;
 import com.bookbud.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -23,9 +24,11 @@ public class DataRestConfig implements RepositoryRestConfigurer {
 
         config.exposeIdsFor(Book.class);
         config.exposeIdsFor(Review.class);
+        config.exposeIdsFor(Message.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
         disableHttpMethods(Review.class, config, theUnsupportedActions);
+        disableHttpMethods(Message.class, config, theUnsupportedActions);
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
                 .allowedOrigins(theAllowedOrigins);
